9a1d4f5a607e253470954fa61bc0cb4c6f7f5d04
SamuelLow <samlow1218@gmail.com>
2020-02-03 20:28:54 -0500

    Added trails to fire

---
+++ Assets/Scripts/fadeOut.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fadeOut : MonoBehaviour
+{
+    private float lifeTime = 1f;
+    public SpriteRenderer spriteRenderer;
+    public float timeForFade = 1f;
+
+    void Start()
+    {
+        lifeTime = timeForFade;
+    }
+    void FixedUpdate()
+    {
+        if(lifeTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            lifeTime -= Time.deltaTime;
+            var alpha = 1 - lifeTime/timeForFade;
+            spriteRenderer.color = Color.Lerp(Color.white, new Color(1,1,1,0), alpha);
+        }
+    }
+}
---
+++ Assets/Scripts/trailSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trailSpawner : MonoBehaviour
+{
+    public GameObject trail;
+    public float spawnRate = .3f;
+    public float spawnTimer = 0;
+    public float startDelay = .1f;
+
+    void Start()
+    {
+        spawnTimer = spawnRate - startDelay;
+    }
+
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+
+
+
+
+        if (spawnTimer >= spawnRate)
+        {
+           Spawn();
+        }
+
+
+    }
+
+
+    void Spawn()
+    {
+        var newTrail = Instantiate(trail);
+        newTrail.transform.position = transform.position;
+        spawnTimer = 0;
+    }
+}
